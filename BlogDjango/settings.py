f96d4be099a1b11ae8e77520c6da4092f515940c
Fabian Taranto <fataranto@gmail.com>
2025-01-26 19:10:28 +0100

    subir imagenes

--- BlogDjango/settings.py
+++ BlogDjango/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "mainapp",
     "pages.apps.PagesConfig",
-    "ckeditor"
+    "ckeditor",
+    "blog.apps.BlogConfig"
 ]
 
 MIDDLEWARE = [
@@ -120,6 +121,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = "static/"
+
+# Media
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "media"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
