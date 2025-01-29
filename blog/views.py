f6b1ef8f99fc2a2dc7569135a1554e67380df024
Fabian Taranto <fataranto@gmail.com>
2025-01-29 19:39:33 +0100

    ajustes articulos y paginacion

--- blog/views.py
+++ blog/views.py
@@ -1,18 +1,33 @@
 from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator
 from blog.models import Category, Article
 
 # Create your views here.
 def list(request):
 
     articles = Article.objects.all()
+    paginator = Paginator(articles, 2)
+
+    page = request.GET.get('page')
+    page_articles = paginator.get_page(page)
 
     return render(request, 'articles/list.html',{
         'title': 'Artículos',
-        'articles': articles
+        'articles': page_articles
     }) 
 
 def category(request, category_id):
     category = get_object_or_404(Category, id=category_id)
+    articles = Article.objects.filter(categories=category_id)
+ 
     return render(request, 'categories/category.html',{
-        'category': category
+        'category': category,
+        'articles': articles
+    })
+
+def article(request, article_id):
+    article = get_object_or_404(Article, id=article_id)
+
+    return render(request, 'articles/detail.html', {
+        'article': article
     })
--- pages/admin.py
+++ pages/admin.py
@@ -1,12 +1,23 @@
 from django.contrib import admin
 from .models import Page
 
+
+# Configuracion extra
+class PageAdmin(admin.ModelAdmin):
+    readonly_fields = ('created_at', 'updated_at')
+    search_fields = ('title', 'content')
+    list_filter = ('visible',)
+    list_display = ('title', 'visible', 'created_at')
+    #ordering = ('-created_at',)
+
+
 # Register your models here.
-admin.site.register(Page)
+admin.site.register(Page, PageAdmin)
 
 # Configuración del panel
 title = "Blog Django"
 subtitle = "Panel de gestión"
+
 admin.site.site_header = title
 admin.site.site_title = title
 admin.site.index_title = subtitle
