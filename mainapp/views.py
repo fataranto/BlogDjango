9b34454ba758d85e49d259cc90cf73d5d3d264d7
Fabian Taranto <fataranto@gmail.com>
2025-01-31 18:39:51 +0100

    formulario de registro basico

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+
+from mainapp.forms import RegisterForm
 
 # Create your views here.
 
@@ -11,4 +14,22 @@ def index(request):
 def about(request):
     return render(request, 'mainapp/about.html', {
         'title': 'Sobre nosotros'
+    })
+
+def register_page(request):
+
+    register_form = RegisterForm()
+
+    if request.method == 'POST':
+        register_form = RegisterForm(request.POST)
+
+        if register_form.is_valid():
+            register_form.save()
+
+            return redirect('inicio')
+
+
+    return render(request, 'users/register.html', {
+        'title': 'Registro',
+        'register_form': register_form
     })
