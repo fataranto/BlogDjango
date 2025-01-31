8fff6d1ce1dcf8dd212005a993a627fde6c2ed06
Fabian Taranto <fataranto@gmail.com>
2025-01-31 20:40:43 +0100

    autenticación completo

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -4,5 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.index, name="index"),
     path('inicio/', views.index, name="inicio"),
-    path('registro/', views.register_page, name="register")
+    path('registro/', views.register_page, name="register"),
+    path('login/', views.login_page, name="login"),
+    path('logout/', views.logout_user, name="logout")
+
 ]
--- pages/views.py
+++ pages/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from .models import Page
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+@login_required(login_url="login") #solo es accesible para usuarios logueados
 def page(request, slug):
     page = Page.objects.get(slug=slug)
 
